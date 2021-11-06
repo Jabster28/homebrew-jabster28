ab5038254192873664f7b4e73a7c75913b3dfb72
Jabster28 <justynboyer@gmail.com>
2021-11-06 20:57:54 +0000

    feat: add decoreco

---
+++ Formula/decoreco.rb
@@ -0,0 +1,13 @@
+class Decoreco < Formula
+  desc "decode and re-encode big media files to save space"
+  homepage "https://github.com/Jabster28/decoreco"
+  url "https://github.com/Jabster28/decoreco/archive/refs/tags/v0.1.0-a.1.tar.gz"
+  sha256 "39516639dc3b74df17a85488353462b44ae327e52808aa46eac3dad889b55ab4"
+  license "CC0-1.0"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", "--root", prefix
+  end
+end
