713c4826e70acdd8155ac8db3c567800e44a8b97
Jabster28 <justynboyer@gmail.com>
2021-11-07 13:00:50 +0000

    feat: add pertrickstence

---
+++ Formula/pertrickstence.rb
@@ -0,0 +1,16 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Pertrickstence < Formula
+  desc "Fake persistence for debain packages"
+  homepage "https://github.com/Jabster28/pertrickstence"
+  url "https://github.com/Jabster28/pertrickstence/archive/refs/tags/v0.1.0-a.1.tar.gz"
+  sha256 "f7c1dac2b5bddede81dc7ca3ced9ae7e0a61479f0779d02c7126cf616612277d"
+  license "CC0-1.0"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", "--locked", "--root", prefix, "--path", "."
+  end
+end
