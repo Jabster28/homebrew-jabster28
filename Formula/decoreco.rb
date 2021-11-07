fd7d4e9ca8a54bb5cce9fe2bf315f449a58eba90
Jabster28 <justynboyer@gmail.com>
2021-11-07 12:42:50 +0000

    fix: oops caps

--- Formula/decoreco.rb
+++ Formula/decoreco.rb
@@ -1,5 +1,5 @@
 class Decoreco < Formula
-  desc "decode and re-encode big media files to save space"
+  desc "Decode and re-encode big media files to save space"
   homepage "https://github.com/Jabster28/decoreco"
   url "https://github.com/Jabster28/decoreco/archive/refs/tags/v0.1.0-a.1.tar.gz"
   sha256 "39516639dc3b74df17a85488353462b44ae327e52808aa46eac3dad889b55ab4"
