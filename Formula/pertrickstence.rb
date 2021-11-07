1844d5fbe9c0827da3c34ffc8bffc72fa6acddd6
Jabster28 <justynboyer@gmail.com>
2021-11-07 15:07:02 +0000

    docs: typo

--- Formula/pertrickstence.rb
+++ Formula/pertrickstence.rb
@@ -1,5 +1,5 @@
 class Pertrickstence < Formula
-  desc "Fake persistence for debain packages"
+  desc "Fake persistence for debian packages"
   homepage "https://github.com/Jabster28/pertrickstence"
   url "https://github.com/Jabster28/pertrickstence/archive/refs/tags/v0.1.0-a.1.tar.gz"
   sha256 "f7c1dac2b5bddede81dc7ca3ced9ae7e0a61479f0779d02c7126cf616612277d"
