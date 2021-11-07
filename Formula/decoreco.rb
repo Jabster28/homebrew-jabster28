56309a61f73cf00c9ea0bf7b7f665c2e5d0f74b1
Jabster28 <29015942+Jabster28@users.noreply.github.com>
2021-11-07 14:05:04 +0000

    decoreco: add 1 bottle.

--- Formula/decoreco.rb
+++ Formula/decoreco.rb
@@ -5,6 +5,13 @@ class Decoreco < Formula
   sha256 "39516639dc3b74df17a85488353462b44ae327e52808aa46eac3dad889b55ab4"
   license "CC0-1.0"
 
+  bottle do
+    root_url "https://github.com/Jabster28/homebrew-jabster28/releases/download/decoreco-1"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, catalina:     "3539f9c2c7c8efbebbcb2252e0d98ddcb9b72df0af9d2cc0820901d85aef701d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "852d8317e988abbb395d09e3c573fe2d0624f3f3acaa74d46ee2f762805a742d"
+  end
+
   depends_on "rust" => :build
   depends_on "ffmpeg"
 
