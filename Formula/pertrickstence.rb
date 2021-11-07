c397096511eb94229f7738c1a167b72c256b5745
Jabster28 <29015942+Jabster28@users.noreply.github.com>
2021-11-07 20:24:37 +0000

    pertrickstence: add 1 bottle.

--- Formula/pertrickstence.rb
+++ Formula/pertrickstence.rb
@@ -5,6 +5,13 @@ class Pertrickstence < Formula
   sha256 "f7c1dac2b5bddede81dc7ca3ced9ae7e0a61479f0779d02c7126cf616612277d"
   license "CC0-1.0"
 
+  bottle do
+    root_url "https://github.com/Jabster28/homebrew-jabster28/releases/download/pertrickstence-1"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, catalina:     "f3bdadd12c9e6deb70d2ff0c13069d608173caf53d16062ad56f530950d77e3a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "0529ec4069ff546886c246beff7d53e7581545e9f8a396364057aba301a83373"
+  end
+
   # requires nightly rust
 
   depends_on "rustup-init" => :build
